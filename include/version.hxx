93a95887bba59d6b6962379924816bd153d82559
dywoq <aleks.koyf@gmail.com>
2025-07-08 16:48:34 +0300

    [scripts] add version.hxx

---
+++ include/version.hxx
@@ -0,0 +1,12 @@
+#ifndef _DYWOQCXX_VERSION_HXX
+#define _DYWOQCXX_VERSION_HXX
+
+#include "__config.hxx"
+
+#if _DYWOQCXX_VERSION >= 202507
+#    define __dywoqcxx_types_version 202507
+#    define __dywoqcxx_memory_version 202507
+#    define __dywoqcxx_result_version 202507
+#endif
+
+#endif
